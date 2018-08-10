271ba88462eaf932f3171d3af1cc55a59752462e
Ivan Mashintsev <mashintsev@gmail.com>
2018-08-10 17:04:39 +0300

    Added favicon package

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,19 +7,26 @@ import './all.sass'
 import '@atlaskit/css-reset';
 
 const TemplateWrapper = ({ children }) => (
-  <div>
-      <Helmet>
-          <title>HeadlessDev Blog</title>
-      </Helmet>
-    <Helmet title="" />
+    <div>
+        <Helmet>
+            <title>HeadlessDev Blog</title>
+            <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff" />
+        </Helmet>
+        <Helmet title="" />
 
-    <Navbar />
-    <div>{children()}</div>
-  </div>
+        <Navbar />
+        <div>{children()}</div>
+    </div>
 )
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+    children: PropTypes.func,
 }
 
 export default TemplateWrapper
